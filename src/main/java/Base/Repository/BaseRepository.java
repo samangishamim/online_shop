04f3fa580633781366b3eae402ca4981a687f122
samangishamim <samangi624@gmail.com>
2024-01-31 18:22:47 +0330

    add update delete method

--- src/main/java/Base/Repository/BaseRepository.java
+++ src/main/java/Base/Repository/BaseRepository.java
@@ -4,9 +4,12 @@ import Base.Model.BaseEntity;
 
 import java.io.Serializable;
 
-public interface BaseRepository <ID extends Serializable , T extends BaseEntity<ID>>{
+public interface BaseRepository<ID extends Serializable, T extends BaseEntity<ID>> {
     void save(T entity);
 
+    T findById(ID id);
 
+    void update(T entity);
 
+    void delete (ID id);
 }
