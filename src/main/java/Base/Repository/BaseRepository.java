02091f6618f5965dd5799b81eec3ff71c9869588
samangishamim <samangi624@gmail.com>
2024-01-31 18:19:59 +0330

    add save method

--- src/main/java/Base/Repository/BaseRepository.java
+++ src/main/java/Base/Repository/BaseRepository.java
@@ -1,4 +1,12 @@
 package Base.Repository;
 
-public interface BaseRepository {
+import Base.Model.BaseEntity;
+
+import java.io.Serializable;
+
+public interface BaseRepository <ID extends Serializable , T extends BaseEntity<ID>>{
+    void save(T entity);
+
+
+
 }
