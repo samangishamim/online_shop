28d9ed72310dcc421c3c9add280b646cabc9fd6b
samangishamim <samangi624@gmail.com>
2024-01-31 18:09:00 +0330

    add type ID for generic

--- src/main/java/Base/Model/BaseEntity.java
+++ src/main/java/Base/Model/BaseEntity.java
@@ -3,12 +3,14 @@ package Base.Model;
 import lombok.*;
 import lombok.experimental.FieldDefaults;
 
+import java.io.Serializable;
+
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE)
 
-public class BaseEntity <ID>{
+public class BaseEntity <ID extends Serializable>{
     ID id;
 }
