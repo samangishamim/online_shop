d4fbf97135a0deca4e21fc8961c458a77f765592
samangishamim <samangi624@gmail.com>
2024-02-04 23:42:56 +0330

    create connection for database

--- src/main/java/Utility/AplicationContext.java
+++ src/main/java/Utility/AplicationContext.java
@@ -1,4 +1,30 @@
 package Utility;
 
+import connection.DBConfig;
+import repository.admin.AdminsRepository;
+import repository.admin.AdminsRepositoryImpl;
+import service.admin.AdminsService;
+import service.admin.AdminsServiceImpl;
+
+import java.sql.Connection;
+
 public class AplicationContext {
+    private static Connection CONNECTION;
+    private static AdminsRepository ADMINS_REPOSITORY;
+    private static AdminsService ADMINS_SERVICE;
+
+    public  static  void initialize(){
+        if(CONNECTION == null){
+             CONNECTION= DBConfig.getConnection();
+        }
+        if(ADMINS_REPOSITORY == null){
+            ADMINS_REPOSITORY=new AdminsRepositoryImpl(CONNECTION);
+            ADMINS_SERVICE=new AdminsServiceImpl(ADMINS_REPOSITORY);
+        }
+
+    }
+
+    public  static AdminsService getAdminsService(){
+        return  ADMINS_SERVICE;
+    }
 }
