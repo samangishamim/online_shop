988248fba6cf9bc27cebb255eba64cbd07391a53
samangishamim <samangi624@gmail.com>
2024-02-04 22:52:23 +0330

    create connection for connecting to database

--- src/main/java/connection/DBConfig.java
+++ src/main/java/connection/DBConfig.java
@@ -1,4 +1,27 @@
 package connection;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DBConfig {
+
+    private static Connection connection = null;
+
+    public DBConfig() {
+    }
+
+    public static Connection getConnection() {
+        if (connection == null) {
+            String url = "jdbc:postgresql://localhost:5432/shop";
+            String userName = "postgres";
+            String passwrod = "shamim1379";
+            try {
+                connection = DriverManager.getConnection(url, userName, passwrod);
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        return connection;
+    }
 }
