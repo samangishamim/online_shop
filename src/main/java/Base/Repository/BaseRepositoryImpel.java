84b75c15b0515b4c61b8229ad2b694e90c705ee7
samangishamim <samangi624@gmail.com>
2024-02-04 22:04:42 +0330

    add save implement

--- src/main/java/Base/Repository/BaseRepositoryImpel.java
+++ src/main/java/Base/Repository/BaseRepositoryImpel.java
@@ -1,4 +1,50 @@
 package Base.Repository;
 
-public class BaseRepositoryImpel {
+import Base.Model.BaseEntity;
+
+import java.io.Serializable;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public abstract class BaseRepositoryImpel<ID extends Serializable, T extends BaseEntity<ID>>
+        implements BaseRepository<ID, T> {
+
+    private final Connection connection;
+
+    protected BaseRepositoryImpel(Connection connection) {
+        this.connection = connection;
+    }
+
+    @Override
+    public void save(BaseEntity entity) throws SQLException {
+        // todo: INSERT INTO TABLE NAME (FIELD NAME) VALUES (QUESTIONMARKS)
+        String sql = " INSERT INTO " + getTableName() + getFieldName() + " VALUES " + getQuestionMark();
+        try (PreparedStatement ps = connection.prepareStatement(sql)) {
+            setFields(ps);
+            ps.executeQuery();
+        }
+    }
+
+    @Override
+    public BaseEntity findById(Serializable serializable) {
+        return null;
+    }
+
+    @Override
+    public void update(BaseEntity entity) {
+
+    }
+
+    @Override
+    public void delete(Serializable serializable) {
+
+    }
+
+    public  abstract String getTableName();
+
+    public abstract String getQuestionMark();
+
+    public abstract String getFieldName();
+    public abstract String setFields(PreparedStatement ps);
 }
