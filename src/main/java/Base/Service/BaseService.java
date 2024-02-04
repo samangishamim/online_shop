95046c5fa7fe4177436b5fc33e0c5b95afbf85f6
samangishamim <samangi624@gmail.com>
2024-02-04 22:36:48 +0330

    add all exceptions

--- src/main/java/Base/Service/BaseService.java
+++ src/main/java/Base/Service/BaseService.java
@@ -6,11 +6,11 @@ import java.io.Serializable;
 import java.sql.SQLException;
 
 public interface BaseService<ID extends Serializable , T extends BaseEntity <ID>> {
-    void save(T entity) ;
+    void save(T entity) throws SQLException;
 
-    T findById(ID id);
+    T findById(ID id) throws SQLException;
 
-    void update(T entity);
+    void update(T entity) throws SQLException;
 
-    void delete (ID id);
+    void delete (ID id) throws SQLException;
 }
