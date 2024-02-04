700df18324684c597adcdd559afe2c0b9f877e61
samangishamim <samangi624@gmail.com>
2024-02-04 22:36:22 +0330

    add all BaseServiceImpl methods

--- src/main/java/Base/Service/BaseServiceImpl.java
+++ src/main/java/Base/Service/BaseServiceImpl.java
@@ -4,33 +4,34 @@ import Base.Model.BaseEntity;
 import Base.Repository.BaseRepository;
 
 import java.io.Serializable;
+import java.sql.SQLException;
 
-public class BaseServiceImpl <ID extends Serializable , T extends BaseEntity <ID>, R extends BaseRepository<ID , T>>
-        implements  BaseService<ID , T>{
+public class BaseServiceImpl<ID extends Serializable, T extends BaseEntity<ID>, R extends BaseRepository<ID, T>>
+        implements BaseService<ID, T> {
 
-    public  final  R repository;
+    public final R repository;
 
     public BaseServiceImpl(R repository) {
         this.repository = repository;
     }
 
     @Override
-    public void save(T entity) {
-
+    public void save(T entity) throws SQLException {
+        repository.save(entity);
     }
 
     @Override
-    public T findById(ID id) {
-        return null;
+    public T findById(ID id) throws SQLException {
+        return repository.findById(id);
     }
 
     @Override
-    public void update(T entity) {
-
+    public void update(T entity) throws SQLException {
+        repository.update(entity);
     }
 
     @Override
-    public void delete(ID id) {
-
+    public void delete(ID id) throws SQLException {
+        repository.delete(id);
     }
 }
