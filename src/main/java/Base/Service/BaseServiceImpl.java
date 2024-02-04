0b98da71309761eab3820a5e6bb5970cd638b37b
samangishamim <samangi624@gmail.com>
2024-02-04 22:32:18 +0330

    generic class and implement all methods

--- src/main/java/Base/Service/BaseServiceImpl.java
+++ src/main/java/Base/Service/BaseServiceImpl.java
@@ -1,4 +1,29 @@
 package Base.Service;
 
-public class BaseServiceImpl {
+import Base.Model.BaseEntity;
+import Base.Repository.BaseRepository;
+
+import java.io.Serializable;
+
+public class BaseServiceImpl <ID extends Serializable , T extends BaseEntity <ID>, R extends BaseRepository<ID , T>>
+        implements  BaseService<ID , T>{
+    @Override
+    public void save(T entity) {
+
+    }
+
+    @Override
+    public T findById(ID id) {
+        return null;
+    }
+
+    @Override
+    public void update(T entity) {
+
+    }
+
+    @Override
+    public void delete(ID id) {
+
+    }
 }
