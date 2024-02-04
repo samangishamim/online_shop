0798ef20438cf12cd5273a907dd9f49d0cde0ac8
samangishamim <samangi624@gmail.com>
2024-02-04 23:22:54 +0330

    fix bug

--- src/main/java/Base/Repository/BaseRepositoryImpl.java
+++ src/main/java/Base/Repository/BaseRepositoryImpl.java
@@ -63,14 +63,14 @@ public abstract class BaseRepositoryImpl<ID extends Serializable, T extends Base
     }
 
     public abstract String getTableName();
+    public abstract String getFieldName();
 
     public abstract String getQuestionMark();
 
-    public abstract String getFieldName();
 
     public abstract String getUpdateFields();
 
-    public abstract String setFields(PreparedStatement ps, T entity, boolean isCountOnly);
+    public abstract void setFields(PreparedStatement ps, T entity, boolean isCountOnly) throws SQLException;
 
-    public abstract T mapResultSetToEntity(ResultSet resultSet);
+    public abstract T mapResultSetToEntity(ResultSet resultSet) throws SQLException;
 }
