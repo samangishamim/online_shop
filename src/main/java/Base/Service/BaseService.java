8f8f284eb89b7b7322379b84a3d06fef3ab01fde
samangishamim <samangi624@gmail.com>
2024-02-04 22:29:19 +0330

    add all methods

--- src/main/java/Base/Service/BaseService.java
+++ src/main/java/Base/Service/BaseService.java
@@ -1,4 +1,16 @@
 package Base.Service;
 
-public interface BaseService {
+import Base.Model.BaseEntity;
+
+import java.io.Serializable;
+import java.sql.SQLException;
+
+public interface BaseService<ID extends Serializable , T extends BaseEntity <ID>> {
+    void save(T entity) ;
+
+    T findById(ID id);
+
+    void update(T entity);
+
+    void delete (ID id);
 }
