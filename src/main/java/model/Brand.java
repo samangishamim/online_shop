9204a24873e56cf2198b75a9ec814d63bb7af404
samangishamim <samangi624@gmail.com>
2024-02-05 15:04:40 +0330

    add lombok annotation and add constructor

--- src/main/java/model/Brand.java
+++ src/main/java/model/Brand.java
@@ -1,4 +1,20 @@
 package model;
 
-public class Brand {
+import Base.Model.BaseEntity;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class Brand extends BaseEntity<Integer> {
+    String brandName;
+
+    public  Brand (int id,String brandName ){
+        super(id);
+        this.brandName=brandName;
+    }
 }
