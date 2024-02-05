f1751984f9df5d99c14c077a80aaf76a2129362a
samangishamim <samangi624@gmail.com>
2024-02-05 19:43:12 +0330

    add connection for product service and repository and create constructor for get product service

--- src/main/java/Utility/ApplicationContext.java
+++ src/main/java/Utility/ApplicationContext.java
@@ -7,12 +7,16 @@ import repository.brand.BrandRepository;
 import repository.brand.BrandRepositoryImpl;
 import repository.category.CategoryRepository;
 import repository.category.CategoryRepositoryImpl;
+import repository.product.ProductRepository;
+import repository.product.ProductRepositoryImpl;
 import service.admin.AdminsService;
 import service.admin.AdminsServiceImpl;
 import service.brand.BrandService;
 import service.brand.BrandServiceImpl;
 import service.category.CategoryServiceImpl;
 import service.category.CategoryService;
+import service.product.ProductService;
+import service.product.ProductServiceImpl;
 
 import java.sql.Connection;
 
@@ -23,7 +27,9 @@ public class ApplicationContext {
     private static BrandRepository BRAND_REPOSITORY;
     private static BrandService BRAND_SERVICE;
     private static CategoryRepository CATEGORY_REPOSITORY;
-    private  static  CategoryService CATEGORY_SERVICE;
+    private static CategoryService CATEGORY_SERVICE;
+    private static ProductRepository PRODUCT_REPOSITORY;
+    private static ProductService PRODUCT_SERVICE;
 
     public static void initialize() {
         if (CONNECTION == null) {
@@ -37,9 +43,13 @@ public class ApplicationContext {
             BRAND_REPOSITORY = new BrandRepositoryImpl(CONNECTION);
             BRAND_SERVICE = new BrandServiceImpl(BRAND_REPOSITORY);
         }
-        if (CATEGORY_REPOSITORY==null){
-            CATEGORY_REPOSITORY=new CategoryRepositoryImpl(CONNECTION);
-            CATEGORY_SERVICE=new CategoryServiceImpl(CATEGORY_REPOSITORY);
+        if (CATEGORY_REPOSITORY == null) {
+            CATEGORY_REPOSITORY = new CategoryRepositoryImpl(CONNECTION);
+            CATEGORY_SERVICE = new CategoryServiceImpl(CATEGORY_REPOSITORY);
+        }
+        if (PRODUCT_REPOSITORY==null){
+            PRODUCT_REPOSITORY=new ProductRepositoryImpl(CONNECTION);
+            PRODUCT_SERVICE=new ProductServiceImpl(PRODUCT_REPOSITORY);
         }
     }
 
@@ -51,7 +61,11 @@ public class ApplicationContext {
         return BRAND_SERVICE;
     }
 
-    public static CategoryService getCategoryService(){
+    public static CategoryService getCategoryService() {
         return CATEGORY_SERVICE;
+    }
+
+    public  static  ProductService getProductService(){
+        return  PRODUCT_SERVICE;
     }
 }
