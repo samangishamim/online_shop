4266513ff92167d33aed3593f3aa751ee975632f
samangishamim <samangi624@gmail.com>
2024-02-05 16:33:48 +0330

    extend baseservice to the class

---
+++ src/main/java/service/category/categoryService.java
@@ -0,0 +1,8 @@
+package service.category;
+
+import Base.Service.BaseService;
+import model.Category;
+import repository.category.CategoryRepository;
+
+public interface categoryService extends BaseService<Integer, Category> {
+}
