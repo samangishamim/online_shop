b1972e2677297437febb7823148bee9c9a0ea140
samangishamim <samangi624@gmail.com>
2024-02-05 16:45:12 +0330

    rename the class

---
+++ src/main/java/service/category/CategoryService.java
@@ -0,0 +1,7 @@
+package service.category;
+
+import Base.Service.BaseService;
+import model.Category;
+
+public interface CategoryService extends BaseService<Integer, Category> {
+}
--- src/main/java/service/category/categoryService.java
+++
@@ -1,8 +0,0 @@
-package service.category;
-
-import Base.Service.BaseService;
-import model.Category;
-import repository.category.CategoryRepository;
-
-public interface categoryService extends BaseService<Integer, Category> {
-}
