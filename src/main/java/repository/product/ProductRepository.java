b2fc3ad572bc25ebb1f89ef345a1a213e86d0302
samangishamim <samangi624@gmail.com>
2024-02-05 18:48:32 +0330

    add arraylist to get list of product

--- src/main/java/repository/product/ProductRepository.java
+++ src/main/java/repository/product/ProductRepository.java
@@ -3,5 +3,9 @@ package repository.product;
 import Base.Repository.BaseRepository;
 import model.Product;
 
+import java.sql.SQLException;
+import java.util.ArrayList;
+
 public interface ProductRepository extends BaseRepository<Integer, Product> {
+    public ArrayList<Product> listOfProduct() throws SQLException;
 }
