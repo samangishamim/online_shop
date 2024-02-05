c90af688118f836c3be29e507810f49122a0a55d
samangishamim <samangi624@gmail.com>
2024-02-05 20:08:09 +0330

    add validation for checking password

--- src/main/java/Utility/Validation.java
+++ src/main/java/Utility/Validation.java
@@ -1,4 +1,16 @@
 package Utility;
 
-public class Validation {
+import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Validation { private static final String PASSWORD_PATTERN = "^(?=.*[a-z])(?=.*[A-Z])" +
+        "(?=.*[0-9])(?=.*[@#!%&*])[A-Za-z0-9@#!%&*]{8,10}$";
+
+    public static boolean checkPassword(String password) {
+        Pattern pattern = Pattern.compile(PASSWORD_PATTERN);
+        Matcher matcher = pattern.matcher(password);
+        return matcher.matches();
+    }
+
 }
