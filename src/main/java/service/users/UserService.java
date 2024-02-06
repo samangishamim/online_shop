d0da5e1a37925be6899a1a08169e1aaa8ab5da7b
samangishamim <samangi624@gmail.com>
2024-02-06 21:07:25 +0330

    fix bug

--- src/main/java/service/users/UserService.java
+++ src/main/java/service/users/UserService.java
@@ -3,5 +3,10 @@ package service.users;
 import Base.Service.BaseService;
 import model.Users;
 
+import java.sql.SQLException;
+
 public interface UserService extends BaseService<Integer, Users> {
+    public void SignUp() throws SQLException;
+    public boolean doSigning (String username,String password) throws SQLException;
+    public boolean signing() throws SQLException;
 }
