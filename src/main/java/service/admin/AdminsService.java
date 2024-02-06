00235ea8a61bad26a1f4a7d851707d56191e3b99
samangishamim <samangi624@gmail.com>
2024-02-06 21:07:07 +0330

    add three methods

--- src/main/java/service/admin/AdminsService.java
+++ src/main/java/service/admin/AdminsService.java
@@ -3,5 +3,10 @@ package service.admin;
 import Base.Service.BaseService;
 import model.Admins;
 
+import java.sql.SQLException;
+
 public interface AdminsService extends BaseService<Integer, Admins> {
+    public void SignUp() throws SQLException;
+    public boolean doSigning (String username,String password) throws SQLException;
+    public boolean signing() throws SQLException;
 }
