950b661ed97391cbc11348f940eda3b78ef7461f
samangishamim <samangi624@gmail.com>
2024-02-06 21:12:46 +0330

    add connection for ShoppingCartRepository and ShoppingCartService

--- src/main/java/Utility/ApplicationContext.java
+++ src/main/java/Utility/ApplicationContext.java
@@ -9,6 +9,10 @@ import repository.category.CategoryRepository;
 import repository.category.CategoryRepositoryImpl;
 import repository.product.ProductRepository;
 import repository.product.ProductRepositoryImpl;
+import repository.shoppingcart.ShoppingCartRepository;
+import repository.shoppingcart.ShoppingCartRepositoryImpl;
+import repository.users.UsersRepository;
+import repository.users.UsersRepositoryImpl;
 import service.admin.AdminsService;
 import service.admin.AdminsServiceImpl;
 import service.brand.BrandService;
@@ -17,6 +21,10 @@ import service.category.CategoryServiceImpl;
 import service.category.CategoryService;
 import service.product.ProductService;
 import service.product.ProductServiceImpl;
+import service.shoppingcart.ShoppingCartService;
+import service.shoppingcart.ShoppingCartServiceImpl;
+import service.users.UserService;
+import service.users.UserServiceImpl;
 
 import java.sql.Connection;
 
@@ -30,6 +38,11 @@ public class ApplicationContext {
     private static CategoryService CATEGORY_SERVICE;
     private static ProductRepository PRODUCT_REPOSITORY;
     private static ProductService PRODUCT_SERVICE;
+    private static UsersRepository USERS_REPOSITORY;
+    private static UserService USER_SERVICE;
+
+    private   static ShoppingCartRepository SHOPPING_CART_REPOSITORY;
+    private    static ShoppingCartService SHOPPING_CART_SERVICE;
 
     public static void initialize() {
         if (CONNECTION == null) {
@@ -47,10 +60,20 @@ public class ApplicationContext {
             CATEGORY_REPOSITORY = new CategoryRepositoryImpl(CONNECTION);
             CATEGORY_SERVICE = new CategoryServiceImpl(CATEGORY_REPOSITORY);
         }
-        if (PRODUCT_REPOSITORY==null){
-            PRODUCT_REPOSITORY=new ProductRepositoryImpl(CONNECTION);
-            PRODUCT_SERVICE=new ProductServiceImpl(PRODUCT_REPOSITORY);
+        if (PRODUCT_REPOSITORY == null) {
+            PRODUCT_REPOSITORY = new ProductRepositoryImpl(CONNECTION);
+            PRODUCT_SERVICE = new ProductServiceImpl(PRODUCT_REPOSITORY);
         }
+        if (USERS_REPOSITORY == null) {
+            USERS_REPOSITORY = new UsersRepositoryImpl(CONNECTION);
+            USER_SERVICE = new UserServiceImpl(USERS_REPOSITORY);
+        }
+        if (SHOPPING_CART_REPOSITORY == null) {
+            SHOPPING_CART_REPOSITORY = new ShoppingCartRepositoryImpl(CONNECTION);
+            SHOPPING_CART_SERVICE = new ShoppingCartServiceImpl(SHOPPING_CART_REPOSITORY);
+        }
+
+
     }
 
     public static AdminsService getAdminsService() {
@@ -65,7 +88,15 @@ public class ApplicationContext {
         return CATEGORY_SERVICE;
     }
 
-    public  static  ProductService getProductService(){
-        return  PRODUCT_SERVICE;
+    public static ProductService getProductService() {
+        return PRODUCT_SERVICE;
+    }
+
+    public static  UserService getUserService(){
+        return  USER_SERVICE;
+    }
+
+    public static ShoppingCartService getShoppingCartService() {
+        return SHOPPING_CART_SERVICE;
     }
 }
