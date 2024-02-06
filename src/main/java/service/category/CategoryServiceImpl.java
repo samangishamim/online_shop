b90f90ae8483e76ae0bbad833df284a68f196d55
samangishamim <samangi624@gmail.com>
2024-02-06 23:18:11 +0330

    fix bug

--- src/main/java/service/category/CategoryServiceImpl.java
+++ src/main/java/service/category/CategoryServiceImpl.java
@@ -4,7 +4,7 @@ import Base.Service.BaseServiceImpl;
 import model.Category;
 import repository.category.CategoryRepository;
 
-public class CategoryServiceImpl extends BaseServiceImpl<Integer, Category, CategoryRepository> implements categoryService{
+public class CategoryServiceImpl extends BaseServiceImpl<Integer, Category, CategoryRepository> implements CategoryService {
     public CategoryServiceImpl(CategoryRepository repository) {
         super(repository);
     }
