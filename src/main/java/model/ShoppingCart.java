bc7c3563570e56547ce8857d3df67dce7c54b306
samangishamim <samangi624@gmail.com>
2024-02-06 19:25:23 +0330

    add lombok annotation and add constructor

--- src/main/java/model/ShoppingCart.java
+++ src/main/java/model/ShoppingCart.java
@@ -1,6 +1,33 @@
 package model;
 
 import Base.Model.BaseEntity;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
 
 public class ShoppingCart extends BaseEntity<Integer> {
+    int productId;
+    int quantity;
+    BigDecimal price;
+    int totalAmount;
+    Date orderDate;
+    int userId;
+
+    public ShoppingCart (int id,int productId,int quantity,BigDecimal price,int totalAmount,Date orderDate,int userId){
+        super(id);
+        this.productId=productId;
+        this.quantity=quantity;
+        this.price=price;
+        this.orderDate=orderDate;
+        this.userId=userId;
+    }
 }
