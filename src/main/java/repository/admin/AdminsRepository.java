20f311159998701392a4e1e3dc91a4ef767094d3
samangishamim <samangi624@gmail.com>
2024-02-06 23:16:39 +0330

    fix bug

--- src/main/java/repository/admin/AdminsRepository.java
+++ src/main/java/repository/admin/AdminsRepository.java
@@ -4,7 +4,9 @@ import Base.Repository.BaseRepository;
 import model.Admins;
 import model.Category;
 
+import java.sql.SQLException;
 
 
 public interface AdminsRepository extends BaseRepository<Integer, Admins> {
+    public boolean doSigning(String username, String password) throws SQLException;
 }
