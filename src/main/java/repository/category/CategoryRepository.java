ae00984bd8d659704ce7fb7e382294113da56799
samangishamim <samangi624@gmail.com>
2024-02-06 23:16:53 +0330

    fix bug

--- src/main/java/repository/category/CategoryRepository.java
+++ src/main/java/repository/category/CategoryRepository.java
@@ -3,9 +3,8 @@ package repository.category;
 import Base.Repository.BaseRepository;
 import model.Category;
 
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 public interface CategoryRepository extends BaseRepository<Integer, Category> {
-
-    public ArrayList<Category>listOfCategory();
 }
