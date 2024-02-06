ce2c664480aee3a55e067b9378da21db02880c3a
samangishamim <samangi624@gmail.com>
2024-02-06 21:06:44 +0330

    fix bug

--- src/main/java/service/users/UserServiceImpl.java
+++ src/main/java/service/users/UserServiceImpl.java
@@ -1,23 +1,63 @@
 package service.users;
 
 import Base.Service.BaseServiceImpl;
+import Utility.Validation;
 import model.Users;
 import repository.users.UsersRepository;
 
 import java.sql.SQLException;
+import java.util.Scanner;
 
 public class UserServiceImpl extends BaseServiceImpl<Integer, Users, UsersRepository> implements UserService{
+    Scanner scanner=new Scanner(System.in);
     public UserServiceImpl(UsersRepository repository) {
         super(repository);
     }
 
     @Override
     public void SignUp() throws SQLException {
+        System.out.println("**** signup ****");
+        System.out.println("enter your username: ");
+        String username = scanner.nextLine();
+        System.out.println("enter your password");
+        String password = getPassword();
 
+        try {
+            repository.save(new Users(username,password));
+            System.out.println("signup is done");
+        } catch (SQLException e) {
+            System.out.println("error" + e.getMessage());
+        }
     }
 
     @Override
-    public boolean signIn() throws SQLException {
-        return false;
+    public boolean doSigning(String username,String password) throws SQLException {
+       return repository.doSigning(username, password);
+    }
+
+    @Override
+    public boolean signing() throws SQLException {
+        System.out.println("***** sign in *****");
+        System.out.println("enter your username: ");
+        String username = scanner.nextLine();
+        System.out.println("enter your password ");
+        String password = scanner.nextLine();
+
+
+        return doSigning(username,password);
+    }
+
+    private String getPassword() {
+        String password;
+        while (true) {
+            System.out.println("enter your password: ");
+            password = scanner.nextLine();
+            boolean checkPassword = Validation.checkPassword(password);
+            if (checkPassword)
+                break;
+            else
+                System.out.println("this is not strong password");
+        }
+        return password;
     }
 }
