26fd8bfc0ad823bca1bbeed6d387403ffce70cbc
samangishamim <samangi624@gmail.com>
2024-02-07 21:56:34 +0330

    fix bug

--- src/main/java/service/brand/BrandService.java
+++ src/main/java/service/brand/BrandService.java
@@ -3,5 +3,9 @@ package service.brand;
 import Base.Service.BaseService;
 import model.Brand;
 
+import java.sql.SQLException;
+import java.util.ArrayList;
+
 public interface BrandService extends BaseService<Integer, Brand> {
+    public ArrayList<Brand> listOfBrand() throws SQLException;
 }
--- src/main/java/service/brand/BrandServiceImpl.java
+++ src/main/java/service/brand/BrandServiceImpl.java
@@ -4,9 +4,17 @@ import Base.Service.BaseServiceImpl;
 import model.Brand;
 import repository.brand.BrandRepository;
 
+import java.sql.SQLException;
+import java.util.ArrayList;
+
 public class BrandServiceImpl extends BaseServiceImpl<Integer, Brand, BrandRepository>
         implements BrandService {
     public BrandServiceImpl(BrandRepository repository) {
         super(repository);
+    }
+
+    @Override
+    public ArrayList<Brand> listOfBrand() throws SQLException {
+        return repository.listOfBrand();
     }
 }
