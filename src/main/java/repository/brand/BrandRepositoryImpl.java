ee6f3ab25e23b76b0cee3f7e659625458e365dab
samangishamim <samangi624@gmail.com>
2024-02-07 19:33:01 +0330

    add method for get list of brand

--- src/main/java/repository/brand/BrandRepositoryImpl.java
+++ src/main/java/repository/brand/BrandRepositoryImpl.java
@@ -2,11 +2,13 @@ package repository.brand;
 
 import Base.Repository.BaseRepositoryImpl;
 import model.Brand;
+import model.Product;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 public class BrandRepositoryImpl extends BaseRepositoryImpl<Integer, Brand> implements BrandRepository {
     public BrandRepositoryImpl(Connection connection) {
@@ -42,5 +44,22 @@ public class BrandRepositoryImpl extends BaseRepositoryImpl<Integer, Brand> impl
     public Brand mapResultSetToEntity(ResultSet resultSet) throws SQLException {
         String brandName = resultSet.getString(1);
         return  new Brand(brandName);
+    }
+
+    @Override
+    public ArrayList<Brand> listOfBrand() throws SQLException {
+        //todo SELECT  * FROM brand ;
+        String sql = "SELECT * FROM brands";
+        try (PreparedStatement ps = connection.prepareStatement(sql,
+                ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY)) {
+            ResultSet resultSet = ps.executeQuery();
+
+            ArrayList<Brand> brandList = new ArrayList<>();
+            while (resultSet.next()) {
+                brandList.add(mapResultSetToEntity(resultSet));
+            }
+            return brandList;
+        }
+    }
     }
 }
