1a12a8c939f4f03fb55487d1ae12de6391dd4691
samangishamim <samangi624@gmail.com>
2024-02-07 19:33:12 +0330

    add arraylist for get list of brand

--- src/main/java/repository/brand/BrandRepository.java
+++ src/main/java/repository/brand/BrandRepository.java
@@ -2,6 +2,11 @@ package repository.brand;
 
 import Base.Repository.BaseRepository;
 import model.Brand;
+import model.Product;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
 
 public interface BrandRepository extends BaseRepository<Integer, Brand> {
+    public ArrayList<Brand> listOfBrand() throws SQLException;
 }
